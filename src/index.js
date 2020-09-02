c5b7f4506e0d6a1f4a04ee03f9d86e6ee2919bd1
sunimalherath <sunimaldv@gmail.com>
2020-09-03 00:06:42 +1000

    displaying seasons based on lat and month

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 // const App = () => {
 //     window.navigator.geolocation.getCurrentPosition(position => console.log(position), err => console.log(err));
@@ -8,22 +9,47 @@ import ReactDOM from 'react-dom';
 // }
 
 class App extends React.Component {
-    // not required in react but is specific to javascript and can use it to execute something first
+    // 1. First method of declare and initialize the state prop with constructor() 
+    
+    
+    /*    
+    // not required in react but is specific to javascript and can use it to execute something first    
     constructor(props) {
         // initialize state (one way of initializing state)
         super(props);
 
         // we only use [this.state = ] ONLY when initializing state. to set state use setState
-        this.state = { lat: null }; // {} is the state object with a key value pair. null -> default value
+        this.state = { lat: null, errorMessage: '' }; // {} is the state object with a key value pair. null -> default value
 
-        window.navigator.geolocation.getCurrentPosition(position => {
-            // update state with setState
-            this.setState({ lat: position.coords.latitude });    
-        }, err => console.log(err));
+        
     }
+    */
+
+    // 2. Second way of declare and initialize the state property - Babel generate the above code for the this one.
+    state = { lat: null, errorMessage: ''};
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }), 
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    // componentDidUpdate() {
+    //     console.log('Component was just updated -> then render() called');
+    // }
     // render method is compulsory
     render() {        
-        return <div>Latitude: { this.state.lat }</div>;
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if(!this.state.errorMessage && this.state.lat) {
+            //return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+
+        return <div>Loading...</div>
     }
 }
 
