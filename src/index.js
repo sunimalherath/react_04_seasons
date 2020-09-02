de0f93a0e09ca173db3f0d2c1f8179cdcd9cb9bc
sunimalherath <sunimaldv@gmail.com>
2020-09-02 22:36:33 +1000

    displaying latitude on screen

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return <div>Hello!</div>;
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(position => console.log(position), err => console.log(err));
+    
+//     return <div>Latitude: </div>; // Problem is getting the latitude value of the 'position' here. That's why the class components are used.
+// }
+
+class App extends React.Component {
+    // not required in react but is specific to javascript and can use it to execute something first
+    constructor(props) {
+        // initialize state (one way of initializing state)
+        super(props);
+
+        // we only use [this.state = ] ONLY when initializing state. to set state use setState
+        this.state = { lat: null }; // {} is the state object with a key value pair. null -> default value
+
+        window.navigator.geolocation.getCurrentPosition(position => {
+            // update state with setState
+            this.setState({ lat: position.coords.latitude });    
+        }, err => console.log(err));
+    }
+    // render method is compulsory
+    render() {        
+        return <div>Latitude: { this.state.lat }</div>;
+    }
 }
 
 ReactDOM.render(
