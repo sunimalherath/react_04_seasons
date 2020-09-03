e67fd381821a840d722e21d0d52d594382e450f3
sunimalherath <sunimaldv@gmail.com>
2020-09-03 21:50:34 +1000

    refomatting code

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React from "react";
+
+// seasonConfig - an object with key, value pairs (keys -> summer & winter). Values are objects with key,value pairs
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr, it is cold!",
+    iconName: "snowflake",
+  },
+};
 
 const getSeason = (lat, month) => {
-    if(month > 2 && month < 9) {
-        return lat > 0 ? 'summer' : 'winter';
-    } else {
-        return lat > 0 ? 'winter' : 'summer';
-    }
-}
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
 
 const SeasonDisplay = (props) => {
-    const season = getSeason(props.lat, new Date().getMonth());
+  const season = getSeason(props.lat, new Date().getMonth());
 
-    const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
-    const icon = season === 'winter' ? 'snowflake' : 'sun'; // semantic-ui icon names
-    return(
-        <div>
-            <i className={`${icon} icon`}/>
-            <h1>{text}</h1>
-            <i className={`${icon} icon`} />
-        </div>
-    ); 
-}
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div>
+      <i className={`massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`massive ${iconName} icon`} />
+    </div>
+  );
+};
 
 export default SeasonDisplay;
